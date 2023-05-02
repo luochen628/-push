b03c1bbae7bc3e16ac353c2b3232c8d73f34bb50
luochen628 <132374545+luochen628@users.noreply.github.com>
2023-05-02 23:45:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,17 +1,5 @@
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	KoboDlfFrFfKH7XuCPlbCf2STOySpT4g4LKg81rgNNI',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onZBP5pc116OZrQv209MqXW9RLpE',
-    }
-  ],
-
-}
 
 module.exports = USER_CONFIG
 
